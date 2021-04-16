bccbb2d4cc3d8a0da40c1c5a8f094c73a5cda4a4
jryoungw <et2035@naver.com>
2021-04-17 05:26:17 +0900

    20210417 First push

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='lightdicom',
+      version='2',
+      url='https://github.com/jryoungw/lightdicom',
+      license='MIT',
+      author='Ryoungwoo Jang',
+      author_email='jryoungw2035@gmail.com',
+      description='LightDICOM package',
+      long_description=open('README.md').read(),
+      zip_safe=False)
